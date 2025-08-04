5a4ae5d4e136ce80541056ab82fd763ca7f9fed5
Nessieme <165574963+Nessieme@users.noreply.github.com>
2025-08-04 21:16:08 +0530

    Add files via upload

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\movie-recommendation-system-web-application-master\movie-recommendation-system-web-application-master\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
